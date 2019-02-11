151167300b0b5360d734f6f1aff34c82c6b006f8
David Ringuet <shelbydav6@gmail.com>
2019-02-11 14:22:59 -0500

    done some things gotta leave quick

--- TP1Partie2/src/Commande.java
+++ TP1Partie2/src/Commande.java
@@ -1,5 +1,41 @@
-
+//----------------------------------------------------
+//Programmeur: David Ringuet
+//Date: 04-02-2019
+//Fichier Plats.java
+//
+//Description de la classe: 
+//Cette classe permet de crééer un objet qui représente un client et les plats qu'il commande. 
+//Cette classe assume que l'entrée est vérifié avant et ne contiend pas d'erreur.
+//
+//----------------------------------------------------
 public class Commande {
-	String nomClient, nomPlat;
+	String nomClient;
 	int qte;
+	Plats plat;
+
+	public Commande( String nom ) {
+		setClient( nom );
+	}
+
+	public void setClient( String nom ) {
+		this.nomClient = nom;
+	}
+
+	public void setQte( int qte ) {
+		this.qte = qte;
+	}
+
+	public void setPlat( String nom, double prix ) {
+		this.plat = new Plats( nom, prix );
+	}
+
+	public void setPlat( Plats plat ) {
+		this.plat = plat;
+	}
+	
+	@
+	public String toString() {
+		return nomClient;
+		
+	}
 }
--- TP1Partie2/src/Plats.java
+++ TP1Partie2/src/Plats.java
@@ -1,17 +1,32 @@
-
+//----------------------------------------------------
+//Programmeur: David Ringuet
+//Date: 04-02-2019
+//Fichier Plats.java
+//
+//Description de la classe: [description de la classe
+//
+//----------------------------------------------------
 public class Plats {
 	private String nom;
 	private double prix;
-	
-	public Plats (String nom, double prix) {
-		
+
+	public Plats( String nom, double prix ) {
+
 	}
-	
+
 	public String getNom() {
 		return this.nom;
 	}
+
 	public double getPrix() {
 		return this.prix;
 	}
 	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 }
